fdcf5175371421b4dcf6c5f332d5884d9bae2ca6
mamadoujuniorsy <mamadoujunior2002@gmail.com>
2025-02-12 15:04:29 +0000

    modif config

--- sav_ssi/next.config.ts
+++ sav_ssi/next.config.ts
@@ -5,6 +5,13 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true, // Désactive les erreurs TypeScript en production
   },
+  webpack(config, { isServer }) {
+    // Ignore les erreurs de module manquant
+    config.ignoreWarnings = [
+      (warning) => warning.module && warning.module.resource && warning.module.resource.includes('node_modules')
+    ];
+    return config;
+  },
 };
 
 export default nextConfig;
