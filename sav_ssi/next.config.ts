6b8957bc3aea9dc5de8e31c18a5afd1cd2e3c9f5
mamadoujuniorsy <mamadoujunior2002@gmail.com>
2025-02-13 08:38:39 +0000

    modif next.config

--- sav_ssi/next.config.ts
+++ sav_ssi/next.config.ts
@@ -3,14 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
-    ignoreBuildErrors: true, // Désactive les erreurs TypeScript en production
+    ignoreBuildErrors: true,
   },
-  webpack(config, { isServer }) {
-    // Ignore les erreurs de module manquant
-    config.ignoreWarnings = [
-      (warning) => warning.module && warning.module.resource && warning.module.resource.includes('node_modules')
-    ];
-    return config;
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
